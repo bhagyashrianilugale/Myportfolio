e614c976e3ea28d264c3e325e6ed5b5316e1de20
bhagyashriugale <bhagyashriugale6970@gmail.com>
2024-04-21 21:09:52 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
+let sideMenu = document.getElementById("sidemenu");
+const scriptURL = "https://script.google.com/macros/s/AKfycbwIpvXTnjwSWOsg0AttCqpmhXGWkMRWYw1H9n2dDUVi8Nm-wu-dF6VxygiEw1H_WYDG/exec"
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+
+console.log(form);
+
+function openTab(tabname) {
+    console.log("clicked");
+    console.log(tabname);
+    if (!tabLinks || !tabContents) {
+        console.error("tabLinks or tabContents is not initialized");
+        return;
+    }
+    
+    for (let i = 0; i < tabLinks.length; i++) {
+            tabLinks[i].classList.remove("active-link");
+        
+    }
+
+    for (let i = 0; i < tabContents.length; i++) {
+            tabContents[i].classList.remove("active-tab");
+        
+    }
+   
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");    
+}
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-200px";
+}
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+          msg.innerHTML = "Message sent successfully";
+           setTimeout(async()=>{
+             msg.innerHTML = "";
+             await form.reset();
+           },3000)
+        })
+      .catch(error => console.error('Error!', error.message))
+  })
